9704d18acf1a7e1fc0252a45b74175cef61e0464
unknown <akilapanunayan@gmail.com>
2024-07-24 20:54:00 +0100

    init

--- components/index.js
+++ components/index.js
@@ -5,6 +5,7 @@ export { default as DashboardSideNav } from './navigation/DashboardSideNav';
 
 // Dashboard
 export { default as ActionCard } from './dashboard/ActionCard';
+export { default as InspectionCard } from './dashboard/InspectionCard';
 
 // Popups
 export { default as AddAction } from './popups/AddAction';
