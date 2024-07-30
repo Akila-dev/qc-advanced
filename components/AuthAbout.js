989b6e41ccb3823c9eba707ae080a96657baa0a0
unknown <akilapanunayan@gmail.com>
2024-07-30 01:52:30 +0100

    user settings page

--- components/AuthAbout.js
+++ components/AuthAbout.js
@@ -3,13 +3,14 @@ import Link from 'next/link';
 
 const AuthAbout = ({ title, subheading, text, linkText, link }) => {
 	return (
-		<div className={`auth-container flex flex-col items-center`}>
-			<div className="mt-[-30px] mb-[25px]">
+		<div className={`lg:p-10 h-screen overflow-auto`}>
+			<div className="h-[15vh] lg:h-auto flex-center text-center lg:pb-7">
 				<h1 className="">{title}</h1>
 			</div>
-			<div className="bg-[--white] w-full rounded-t-[--rounding] md:rounded-[--rounding] md:h-[70vh] h-full py-5 md:py-7 max-w-[800px] shadow shadow-white">
-				<div className="bg-[--white] w-full rounded-t-[--rounding] md:rounded-[--rounding] shadow-[--shadow] h-full px-5 md:px-7  overflow-auto md:pb-3">
-					<div className="flex flex-col items-center space-y-5">
+			<div className="dashboard-content-box max-w-[900px] py-5 lg:py-7 !overflow-hidden">
+				{/* <div className="bg-[--white] w-full rounded-t-[--rounding] md:rounded-[--rounding] shadow-[--shadow] h-full px-5 md:px-7  overflow-auto md:pb-3"> */}
+				<div className="h-full overflow-auto">
+					<div className="flex flex-col items-center space-y-5 px-4 lg:px-7">
 						<h3 className="text-left w-full">{subheading}</h3>
 						<div className="space-y-5">
 							{text.map((p, index) => (
@@ -23,6 +24,8 @@ const AuthAbout = ({ title, subheading, text, linkText, link }) => {
 						</div>
 					</div>
 				</div>
+				{/* </div> */}
+				<div className="popup-pb" />
 			</div>
 		</div>
 	);
